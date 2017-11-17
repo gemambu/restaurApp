e65db03fdd01f60f95a6f243591e0a0ba55d80c5
Gema Martínez <gemambu@gmail.com>
2017-11-17 19:49:51 +0100

    table detail with recycler view

--- app/src/main/java/com/gmb/restaurapp/activity/TableDetailActivity.kt
+++ app/src/main/java/com/gmb/restaurapp/activity/TableDetailActivity.kt
@@ -3,11 +3,11 @@ package com.gmb.restaurapp.activity
 import android.app.AlertDialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.Button
-import android.widget.ListView
 import android.widget.TextView
 import com.gmb.restaurapp.R
 import com.gmb.restaurapp.adapter.DishRecyclerViewAdapter
@@ -21,39 +21,36 @@ class TableDetailActivity : AppCompatActivity() {
         var table: Table? = null
     }
 
+    lateinit var dishList: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_table_detail)
 
         val name = findViewById<TextView>(R.id.table_detail_title)
-        val dishList = findViewById<RecyclerView>(R.id.recycler_view_dish)
-        val calculateBill = findViewById<Button>(R.id.calculate_bill_btn)
+
         findViewById<View>(R.id.calculate_bill_btn).setOnClickListener { showBill() }
+        findViewById<View>(R.id.show_menu_btn).setOnClickListener { showMenu() }
 
         val intent = intent
 
         table = intent.getSerializableExtra("EXTRA_TABLE") as? Table
         name.text = getString(R.string.detail_table_number, table?.number ?: 1)
 
-        dishList.layoutManager = LinearLayoutManager(this)
-
-
-
         table?.addDish(DishOrdered(1, "Dim sum", "dilisius dim sum", 12.5f, 3, null, "con extra de picante"))
         table?.addDish(DishOrdered(2, "Dim sum 2", "dilisius dim sum 2", 14.05f, 3, null, null))
         table?.addDish(DishOrdered(3, "Sopa miso", "sopita pal invierniqui", 9.95f, 3, null, null))
 
+        dishList = findViewById<RecyclerView>(R.id.recycler_view_dish)
         val dishListAdapter = DishRecyclerViewAdapter(table?.dishes)
         dishList.adapter = dishListAdapter
 
-        /*
-        if (fragmentManager.findFragmentById(R.id.table_detail_fragment) == null) {
-            val table = this.intent.getSerializableExtra("EXTRA_TABLE") as? Table
-            val fragment = TableDetailFragment.newInstance(table)
-            fragmentManager.beginTransaction()
-                    .add(R.id.table_detail_fragment, fragment)
-                    .commit()
-        }*/
+        dishList.layoutManager = GridLayoutManager(this, 1)
+        dishList.itemAnimator = DefaultItemAnimator()
+    }
+
+    private fun showMenu() {
+
     }
 
     private fun showBill() {
@@ -64,7 +61,6 @@ class TableDetailActivity : AppCompatActivity() {
                     dialog.dismiss()
 
                 })
-                //.setNegativeButton(getString(R.string.cancel_download), { _, _ ->  finish()})
                 .show()
     }
 }
